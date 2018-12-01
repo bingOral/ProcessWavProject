49a678e1a00603eda2ec1706fd9e2e4847db317f
鄢开拓 <iyoyo@yankaituodeMacBook-Pro.local>
2018-12-01 16:26:47 +0800

    update

--- pro.pl
+++ pro.pl
@@ -4,6 +4,20 @@ use strict;
 use Data::Dumper;
 use script::CallOuterServer;
 
-my $res = OuterServer::callNuanceEnglishAsrEngine('http://li4240789.vicp.io:9000/data/voa/special/vadnn/chinese-businesses-wholesale-capital-struggle-economy-slows/chinese-businesses-wholesale-capital-struggle-economy-slows-034.wav','http://li4240789.vicp.io:8081/v4/jobs');
+#my $res = OuterServer::callNuanceEnglishAsrEngine('http://li4240789.vicp.io:9000/data/voa/special/vadnn/chinese-businesses-wholesale-capital-struggle-economy-slows/chinese-businesses-wholesale-capital-struggle-economy-slows-034.wav','http://li4240789.vicp.io:8081/v4/jobs');
 
-print Dumper($res)."\n";
+#print Dumper($res)."\n";
+
+
+use LWP::UserAgent;
+use JSON;
+
+my $url = 'http://li4240789.vicp.io:8081/v4/jobs/c57a96e0-f504-11e8-ae7f-158de6cf37b1/results';
+my $ua = LWP::UserAgent->new;
+my $req = HTTP::Request->new(GET => $url);
+
+# send request
+my $jsonparser = new JSON;
+my $res = $ua->request($req);
+my $ref = $jsonparser->decode($res->decoded_content);
+print $ref->{channels}->{channel1}->{transcript}->[0]->{text}."\n";
--- script/CallOuterServer.pm
+++ script/CallOuterServer.pm
@@ -11,36 +11,31 @@ sub callNuanceEnglishAsrEngine
 	my $engine_url = shift;
 
 	my $jsonparser = new JSON;
-	my $json_data = {"job_type" => "batch_transcription",
+
+	my $body_data = {"job_type" => "batch_transcription",
 					"channels" => {"channel1" => 
 									{"format" => "audio/wave","result_format" => "transcript"}
 								},
 					"model" => {"name" => "eng-usa","sample_rate" => 16000},
 					"operating_mode" => "accurate"};
-	$json_data->{channels}->{channel1}->{url} = $wavfile;
-	my $json = $jsonparser->encode($json_data);
-	print $json."\n";
+	$body_data->{channels}->{channel1}->{url} = $wavfile;
 
 	#send
 	my $ua = LWP::UserAgent->new;
 	my $req = HTTP::Request->new('POST' => $engine_url); 
 	$req->content_type('application/json-rpc; charset=UTF-8');
-	$req->content($json);
-	my $res = $ua->request($req);
-	my $reference = $jsonparser->decode($res->content())->{reference};
+	$req->content($jsonparser->encode($body_data));
 
-	#get result 
-	my $geturl = $engine_url.'/'.$reference."/results";
-	my $response = $ua->get($geturl);
-	my $asr_res = $jsonparser->decode($response->content);
-	while (1) 
-	{
-		if($asr_res->{channels}->{channel1}->{transcript}->[0]->{text})
-		{
-			return $asr_res->{channels}->{channel1}->{transcript}->[0]->{text};
-			last;
-		}
-	}
+	my $res = $ua->request($req);
+	my $job_id = $jsonparser->decode($res->content())->{reference};
+
+	#get
+	my $results_url = $engine_url.'/'.$job_id.'/results';
+	my $req = HTTP::Request->new(GET => $results_url);
+
+	my $res = $ua->request($req);
+	my $content = $jsonparser->decode($res->decoded_content);
+	return $content->{channels}->{channel1}->{transcript}->[0]->{text};
 }
 
 sub callBaiduEnglishAsrEngine
