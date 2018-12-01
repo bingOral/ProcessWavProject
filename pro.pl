4169238b2ed4dd3f09a3a419fbfa68085c5a8e7c
yankt <yankt@localhost.localdomain>
2018-12-01 04:35:05 -0500

    ok

--- pro.pl
+++ pro.pl
@@ -4,8 +4,10 @@ use strict;
 use JSON;
 use Data::Dumper;
 use LWP::UserAgent;
+use script::CallOuterServer;
 
-OuterServer::callNuanceEnglishAsrEngine('http://192.168.1.20:9000/data/voa/special/vadnn/chinese-businesses-wholesale-capital-struggle-economy-slows/chinese-businesses-wholesale-capital-struggle-economy-slows-034.wav','http://192.168.1.20:8081/v4/jobs');
+my $asr_res = OuterServer::callNuanceEnglishAsrEngine('http://192.168.1.20:9000/data/voa/special/vadnn/chinese-businesses-wholesale-capital-struggle-economy-slows/chinese-businesses-wholesale-capital-struggle-economy-slows-034.wav','http://192.168.1.20:8081/v4/jobs');
+print $asr_res."\n";
 
 1;
 
--- script/CallOuterServer.pm
+++ script/CallOuterServer.pm
@@ -5,18 +5,16 @@ use strict;
 use JSON;
 use LWP::UserAgent;
 
-
 sub callNuanceEnglishAsrEngine
 {
 	my $wavfile = shift;
 	my $engine_url = shift;
 
 	my $res = callNuanceEnglishAsrEngine_request($wavfile,$engine_url);
-	print $res->{result}."\n";
-	print $res->{status}."\n";
+	#print $res->{result}."\n";
+	#print $res->{status}."\n";
 
 	my $asr_res = callNuanceEnglishAsrEngine_get($res);
-	print $asr_res."\n";
 	return $asr_res;
 }
 
@@ -29,11 +27,9 @@ sub callNuanceEnglishAsrEngine_request
 	my $jsonparser = new JSON;
 
 	my $body_data = {"job_type" => "batch_transcription",
-					"channels" => {"channel1" => 
-									{"format" => "audio/wave","result_format" => "transcript"}
-								},
-					"model" => {"name" => "eng-usa","sample_rate" => 16000},
-					"operating_mode" => "accurate"};
+			 "channels" => {"channel1" => {"format" => "audio/wave","result_format" => "transcript"}},
+		            "model" => {"name" => "eng-usa","sample_rate" => 16000},
+		   "operating_mode" => "accurate"};
 	$body_data->{channels}->{channel1}->{url} = $wavfile;
 
 	#send
@@ -52,14 +48,37 @@ sub callNuanceEnglishAsrEngine_request
 
 sub callNuanceEnglishAsrEngine_get
 {
-	my $ref = shift;
-	my $jsonparser = new JSON;
+        my $ref = shift;
+        my $jsonparser = new JSON;
 
-	my $ua = LWP::UserAgent->new;
-	my $req = HTTP::Request->new(GET => $ref->{result});
-	my $res = $ua->request($req);
-	my $content = $jsonparser->decode($res->decoded_content);
-	return $content->{channels}->{channel1}->{transcript}->[0]->{text};
+        my $ref_result;
+        my $ref_status;
+
+        while(1)
+        {   
+                my $ua = LWP::UserAgent->new;
+                my $req_result = HTTP::Request->new(GET => $ref->{result});
+                my $req_status = HTTP::Request->new(GET => $ref->{status});
+
+                my $res_result = $ua->request($req_result);
+                my $res_status = $ua->request($req_status);
+
+                $ref_result = $jsonparser->decode($res_result->decoded_content);
+                $ref_status = $jsonparser->decode($res_status->decoded_content);
+
+                if($ref_status->{status} eq 'TRANSCRIBED')
+                {   
+                        last;
+                }   
+                else
+                {   
+                        #print $ref_status->{status}."\n";
+                        sleep(1);
+                }   
+        }   
+    
+        my $res = $ref_result->{channels}->{channel1}->{transcript}->[0]->{text};
+        return $res;
 }
 
 sub callBaiduEnglishAsrEngine
